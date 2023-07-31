876e728dc190d022cc5f1530780477cdbe11e989
fuyuanhui <Fuyuanhuiabcd@123>
2023-07-31 17:44:10 +0800

    add utils

---
+++ spring-boot-starter-websocket/src/main/java/com/qiuguan/websocket/controller/HtmlToPdfTask.java
@@ -0,0 +1,23 @@
+package com.qiuguan.websocket.controller;
+
+import com.qiuguan.websocket.utils.HtmlToPdfStreamUtils;
+
+import java.io.OutputStream;
+import java.util.concurrent.Callable;
+
+/**
+ * @author fu yuan hui
+ * @date 2023-07-25 14:30:27 Tuesday
+ */
+public class HtmlToPdfTask implements Callable<OutputStream> {
+    private final String html;
+
+    public HtmlToPdfTask(String html) {
+        this.html = html;
+    }
+
+    @Override
+    public OutputStream call() throws Exception {
+        return HtmlToPdfStreamUtils.generatePdfStream(html);
+    }
+}
